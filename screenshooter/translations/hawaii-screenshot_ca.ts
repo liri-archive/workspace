479cbcb4dd0b9995e816a382eb360c1abc6f4cd6
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2016-04-07 20:33:17 +0200

    screenshot: Fetch translations

---
+++ screenshooter/translations/hawaii-screenshot_ca.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ca" version="2.1">
+<context>
+    <name>Screenshooter</name>
+    <message>
+        <location filename="../screenshooter.cpp" line="164"/>
+        <source>Screenshot from %1</source>
+        <translation>Captura de pantalla de %1</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="38"/>
+        <source>Screenshot</source>
+        <translation>Captura de pantalla</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="55"/>
+        <source>Cancel</source>
+        <translation>Cancel·la</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="65"/>
+        <location filename="../qml/main.qml" line="115"/>
+        <source>Take Screenshot</source>
+        <translation>Fes una captura</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="74"/>
+        <source>OK</source>
+        <translation>D&apos;acord</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="129"/>
+        <source>Grab the whole screen</source>
+        <translation>Capta tota la pantalla</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="137"/>
+        <source>Grab the active window</source>
+        <translation>Capta la finestra activa</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="146"/>
+        <source>Select a window</source>
+        <translation>Seleccioneu una finestra</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="155"/>
+        <source>Select area to grab</source>
+        <translation>Seleccioneu l&apos;àrea per captar</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="166"/>
+        <source>Grab after a delay of</source>
+        <translation>Capta després d&apos;un retard de </translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="177"/>
+        <source>seconds</source>
+        <translation>segons</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="188"/>
+        <source>Effects</source>
+        <translation>Efectes</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="195"/>
+        <source>Include pointer</source>
+        <translation>Inclou el punter</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="202"/>
+        <source>Include the window border</source>
+        <translation>Inclou la vora de la finestra</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="215"/>
+        <source>Preview</source>
+        <translation>Previsualitza</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="230"/>
+        <source>Options</source>
+        <translation>Opcions</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="237"/>
+        <source>Save</source>
+        <translation>Desa</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="244"/>
+        <source>Copy to clipboard</source>
+        <translation>Copia-ho al porta-retalls</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="278"/>
+        <source>Save Screenshot As</source>
+        <translation>Desa la captura com a </translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="281"/>
+        <source>Image files (*.jpg *.jpeg *.png)</source>
+        <translation>Fitxers d&apos;imatge (*.jpg *.jpeg *.png)</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="281"/>
+        <source>All files (*)</source>
+        <translation>Tots els fitxers (*)</translation>
+    </message>
+</context>
+</TS>
